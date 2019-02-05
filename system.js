e7bbe06f24413d18ae22daf04077d1c62920b21e
Conrad Koh <conradkoh@gmail.com>
2019-02-06 02:23:16 +0800

    Added configuration file creation for production build

--- system.js
+++ system.js
@@ -9,11 +9,29 @@ System.classes = Classes;
 System.package = {
     name: pkg.name
 }
+System.getConfigFilename = function () {
+    let production_config_filename = '_modules.json';
+    let production_config_filepath = path.resolve(path.join(__dirname, production_config_filename));
+    let development_config_filename = 'modules.json';
+    let developlent_config_filepath = path.resolve(path.join(__dirname, development_config_filename));
+    switch(process.env.NODE_ENV) {
+        case 'production':
+            if(!fs.existsSync(production_config_filepath)) {
+                fs.copyFileSync(developlent_config_filepath, production_config_filepath);
+                alert(`New configuration file created!\n\n${developlent_config_filepath}\n\nModify this file to set up your own configuration!`);
+            };
+            return production_config_filename;
+        default:
+        case 'developlent':
+            return developlent_config_filepath;
+    }
+}
 /**
  * @returns {Object<string, Module>}
  */
 System.getModules = function getModules() {
-    let modules_data = fs.readFileSync('modules.json');
+    let config_filename = System.getConfigFilename();
+    let modules_data = fs.readFileSync(config_filename);
     let module_configs = JSON.parse(modules_data.toString());
     let modules = Object.keys(module_configs).reduce((last, identifier, idx) => {
         let module_config = module_configs[identifier];
